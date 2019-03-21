542a3711a673a787e72b89416f5186936aac42d2
wangxuedi <daxue0929@qq.com>
2019-03-22 01:33:22 +0800

    1.now

---
+++ basic/demo.py
@@ -0,0 +1,12 @@
+# import turtle as t
+#
+# def draw(n):
+#     # t.penup()
+#     # t.goto(0,-n)
+#     # t.pendown()
+#
+#     t.circle(n)
+# for i in range(20,80,20):
+#     draw(i)
+#
+# t.done()
