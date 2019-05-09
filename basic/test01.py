32f2c37e1c466eac76373126b8bd7862eac8436f
Administrator <ainidehuiji@163.com>
2019-05-09 15:50:01 +0800

    提交1

---
+++ basic/test01.py
@@ -0,0 +1,18 @@
+# i = 24
+# while True:
+#     a = int(input('请输入数字:'))
+#     if int(a) > i:
+#         print('太大了')
+#     elif int(a) < i:
+#         print('太小了')
+#     else:
+#         print('猜对了。')
+#         break
+
+for item in [1, 2, 3]:
+    print("123")
+
+for index in range(5, 10):
+    print(index)
+
+
